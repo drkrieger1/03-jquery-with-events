dc6ee753c29b806cc5d32681afde435fc96f30e9
Erik Plyushko <eplyushko@gmail.com>
2017-08-16 14:28:03 -0700

    erik-thomas

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -37,6 +37,7 @@ articleView.handleAuthorFilter = function() {
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
     if ($(this).val()) {
+      // $('#articles').hide();
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
@@ -84,5 +85,5 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
 })
